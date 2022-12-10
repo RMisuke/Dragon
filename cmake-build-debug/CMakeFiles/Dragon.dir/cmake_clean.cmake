4a43c6447fc183d0ec2c86ebaea064ca1136823d
Snow <sinuo0720@icloud.com>
2022-12-10 08:33:43 +0800

    First Git

---
+++ cmake-build-debug/CMakeFiles/Dragon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dragon.dir/main.cpp.o"
+  "Dragon"
+  "Dragon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dragon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
